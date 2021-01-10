4e8f7bb7a00c34c4fabc8eeb5a518a75467de7f3
boizsammy <55563637+boizsammy@users.noreply.github.com>
2021-01-09 17:53:26 -0800

    final edit for now

--- execute.js
+++ execute.js
@@ -1,16 +1,4 @@
 
-/*
-
-const setEmbed = new EmbeddedMap("proofFolder/set.html",500,500,"set");
-setEmbed.submitTag("set")
-.modifyText("h2")
-.moveTextUp(-30);
-
-const setBuilderEmbed = new EmbeddedMap("proofFolder/setBuilder.html",500,500,"setBuilder");
-setBuilderEmbed.submitTag("setBuilder")
-.modifyText("h2")
-.moveTextUp(-10);
-
 const imgObj1 = new ImgClass("showProofImg","imgData1")
 .setUp("imgDataList1","imgMenu1")
 .submitImg("actualImg")
@@ -26,5 +14,5 @@ ideaClass
 .submitIdea()
 .addSubContext()
 .showIdea()
-.addIdea("cartesianProduct")
-*/
+.addIdea("Zell")
+
